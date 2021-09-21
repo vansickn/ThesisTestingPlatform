83e91c127d104483a646c903e1640abb7e856fa9
Noah VanSickle <vansickn@union.edu>
2021-09-21 15:47:16 -0400

    First pass at New dropzone

--- src/views/CreateTest.vue
+++ src/views/CreateTest.vue
@@ -1,11 +1,16 @@
 <template>
-    <h1> Upload your files</h1>
-    <div class="dropContainer">
-    <Dropzone class="dropzone" v-if="!verified1" @drop.prevent="drop1" @change="select1"/>
-    <img v-if="verified1" class="timg" :src="fileURL1" alt="hi" srcset="">
-    <Dropzone class="dropzone" v-if="!verified2" @drop.prevent="drop2" @change="select2"/>
-    <img v-if="verified2" class="timg" :src="fileURL2" alt="" srcset="">
+    <h1 class="mx-auto py-5 text-2xl"> Upload your files</h1>
+
+
+    <div class="w-full flex mx-auto justify-center item-center flex-wrap">
+        <Dropzone class="dropzone" v-if="!verified1" @drop.prevent="drop1" @change="select1"/>
+        <img v-if="verified1" class="shadow-xl md:w-640 md:h-360 sm:w-11/12 w-11/12 mx-5 sm:mb-5 xs:mb-5" :src="fileURL1" alt="" srcset="">
+        <Dropzone class="dropzone" v-if="!verified2" @drop.prevent="drop2" @change="select2"/>
+        <img v-if="verified2" class="shadow-xl md:w-640 md:h-360 sm:w-11/12 w-11/12 mx-5 sm:mb-5 xs:mb-5" :src="fileURL2" alt="" srcset="">
     </div>
+
+
+
     <h3>How large do you want your sample size?</h3>
     <div class="viewership">
         <SampleSizeOption :text="'Option 1'" :activePlan="activePlan" @onUpdatePlan="setActive"/>
@@ -67,6 +72,7 @@ export default {
             this.verifyFileTest(e.dataTransfer.files[0],false)
         },
         select1: function () {
+            console.log(document.querySelector(".dropzoneFile").files)
             this.verifyFileTest(document.querySelector(".dropzoneFile").files[0],true)
         },
         select2: function () {
