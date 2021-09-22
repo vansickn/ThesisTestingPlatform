3f0566fb63285b90d2c336680dd1d0360ff5f310
Noah VanSickle <vansickn@union.edu>
2021-09-22 13:41:46 -0400

    Only loads picker on user data != null

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,12 @@
 <template>
   <h1 class="text-4xl mx-auto py-10 md:text-2xl sm:text-lg sm:pt-5 pb-3 xs:text-sm xs:pt-5"> Which Thumbnail are you more likely to click on? </h1>
-  <Picker/>
+  <Picker v-if="user.data != null"/>
 </template>
 
 <script>
 import Picker from '../components/Picker.vue';
 import firebase from 'firebase';
+import { mapGetters } from 'vuex';
 
 const db = firebase.firestore();
 
@@ -15,7 +16,15 @@ name: "home",
   data() {
     return {};
   },
-  created() {},
+  created() {
+    
+  },
+  computed: {
+    ...mapGetters({
+      userData: 'userData',
+      user: 'user'
+    })
+  },
   mounted() {},
   methods: {},
   watch: {},
