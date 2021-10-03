2f0a379feaf6cc698821f4abac8e73b0234c74a2
Noah VanSickle <vansickn@union.edu>
2021-10-03 00:22:02 -0400

    changed copy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1 class="text-4xl mx-auto py-10 md:text-2xl sm:text-lg sm:pt-5 pb-3 xs:text-sm xs:pt-5"> Which Thumbnail are you more likely to click on? </h1>
+  <h1 class="text-4xl mx-auto py-10 md:text-2xl sm:text-lg sm:pt-5 pb-3 xs:text-sm xs:pt-5"> Which video are you more likely to click on? </h1>
   <Picker v-if="user.data != null"/>
 </template>
 
