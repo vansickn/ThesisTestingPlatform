620eb1fd9280bb806a65d7155dc2809435c8a958
Noah VanSickle <vansickn@union.edu>
2021-10-17 20:52:27 -0400

    added different padding

--- src/components/Picker.vue
+++ src/components/Picker.vue
@@ -4,8 +4,8 @@
             <!-- <img class="img" :src="thumbnail1">
         <img class="img" :src="thumbnail2"> -->
         <!-- Want function to ultimately be selectedThumbnail -->
-        <Thumbnail @onClickedThumbnail="selectThumbnail1" :image="thumbnail1" :userCreated="userCreatedPhoto" :title="title1"/>
-        <Thumbnail @onClickedThumbnail="selectThumbnail2" :image="thumbnail2" :userCreated="userCreatedPhoto" :title="title2"/>
+        <Thumbnail @onClickedThumbnail="selectThumbnail1" :image="thumbnail1" :userCreated="userCreatedPhoto" :title="title1" class="mx-5"/>
+        <Thumbnail @onClickedThumbnail="selectThumbnail2" :image="thumbnail2" :userCreated="userCreatedPhoto" :title="title2" class="mx-5"/>
         <!-- need to change click to image instead of whole thumbnail -->
     </div>
 <!-- going to pass in the user who created the test, and calculate the user photo from here. Could also just calculate that in the home.vue as well and just pass in the photo. Either works -->
