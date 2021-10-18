00b4f230b71e08f095fa14be661e2e03af826308
Noah VanSickle <vansickn@union.edu>
2021-10-17 20:52:55 -0400

    added red and blue colors as legend

--- src/components/Thumbnail.vue
+++ src/components/Thumbnail.vue
@@ -1,7 +1,12 @@
 <template>
 <!-- Div container, flex column, first item is image, second item is another div with flex row, aligned to left -->
-    <div class="md:w-5/12 mx-5 sm:w-100 sm:mb-5 xs:w-100 xs:mb-5 container flex-col">
-        <img class= 'shadow-xl transition duration-300 ease-in-out transform md:hover:scale-105 filter md:hover:brightness-105' @click="clickedThumbnail" @mouseover="hover = true" @mouseleave="hover = false" :class="{'hover': hover}" :src="image" @load="loadImage">
+    <div class="md:w-5/12 sm:w-100 sm:mb-5 xs:w-100 xs:mb-5 container flex-col">
+        <img class= 'shadow-xl transition duration-300 ease-in-out transform md:hover:scale-105 filter md:hover:brightness-105 select-none' @click="clickedThumbnail" @mouseover="hover = true" @mouseleave="hover = false" :src="image" @load="loadImage" 
+        :class="{
+            'hover': hover,
+            'border-4 border-red-500 rounded-md': border == 'red',
+            'border-4 border-blue-500 rounded-md': border == 'blue',
+            }">
         <div class="container flex-row flex"> 
             <img :src="userCreated" class="bg-red-100 md:w-10 md:h-10 rounded-full mt-3 w-8 h-8 mb-0">
             <span class="md:ml-5 ml-3 mt-3 text-sm md:text-base"> {{title}}</span>
@@ -45,6 +50,11 @@ export default {
         loadImage(){
             this.isLoaded = true;
         },
+        borderColor(color){
+            if(color == 'red'){
+                return {'border-4 border-red-500 rounded-md':border != null};
+            }
+        }
     }
 }
 </script>
