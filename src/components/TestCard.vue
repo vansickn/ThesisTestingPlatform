5321165272bc3178a4b91026f16fb2194c3f2a8f
Noah VanSickle <vansickn@union.edu>
2021-10-18 23:42:28 -0400

    mytests #ofthumbs now based on array v-for

--- src/components/TestCard.vue
+++ src/components/TestCard.vue
@@ -8,8 +8,10 @@
             <BarChartTest :id="testID"/>
         </div>
         <div class="w-11/12 grid grid-cols-2 gap-2">
-            <TestCardThumbnail :image="thumbnail1" :userCreated="userData.photo" :title="title1" :border="'red'" class="mt-2"/>
-            <TestCardThumbnail :image="thumbnail2" :userCreated="userData.photo" :title="title2" :border="'blue'" class="mt-2"/>
+        <!-- v-for TestCard Thumbnail with n in range of numberOfImages, pass in both arrays, use prop of n to determine which index to display -->
+            <!-- <TestCardThumbnail :image="thumbnail1" :userCreated="userData.photo" :title="title1" :border="'red'" class="mt-2"/>
+            <TestCardThumbnail :image="thumbnail2" :userCreated="userData.photo" :title="title2" :border="'blue'" class="mt-2"/> -->
+            <TestCardThumbnail v-for="n in numberOfImages" :key="n" :image="image_array[n-1]" :title="title_array[n-1]" :index="n-1" />
         </div>
         <!-- Eventually will change this to grid-cols-2 in order to have a button to extend the sample size -->
         <div class="w-11/12 grid grid-cols-1 my-2 gap-4 text-sm sm:text-base">
@@ -39,6 +41,9 @@ export default {
             thumbnail1: null,
             thumbnail2: null,
             sampleSize: null,
+            numberOfImages: null,
+            image_array: [],
+            title_array: [],
         }
     },
     computed: {
@@ -49,30 +54,36 @@ export default {
     components: {Thumbnail,BarChartTest,TestCardThumbnail},
     props: ['testID'],
     methods: {
-        generateTestData(){
+        async generateTestData(){
             db.collection('CreatedTests').doc(this.testID).onSnapshot((doc) => {
-                this.img1votes = doc.data().img1votes;
-                this.img2votes = doc.data().img2votes;
                 this.sampleSize = doc.data().sampleSize;
                 this.title1 = doc.data().title1;
                 this.title2 = doc.data().title2;
+                this.numberOfImages = doc.data().numberOfImages;
+                this.title_array = doc.data().title_array;
+                this.generateThumbnailsFromTestID();
             })
         },
         generateThumbnailsFromTestID(){
-            storageRef.child('/tests/' + this.testID).listAll().then((res) => {
-                console.log(res)
-                res.items[0].getDownloadURL().then(url => {
-                    this.thumbnail1 = url
-                })
-                res.items[1].getDownloadURL().then(url => {
-                    this.thumbnail2 = url
-                })
-            })
+            console.log("HEYYYYY")
+            console.log(this.numberOfImages)
+            for (let i = 0; i < this.numberOfImages; i++) {
+                const image_no = i+1
+                storageRef.child('/tests/' + this.testID + "/img_" + image_no + "/").listAll().then((res) => {
+                    console.log(res)
+                    res.items[0].getDownloadURL().then(url => {
+                        this.image_array[i] = url
+                        console.log(url)
+                    })
+                })                
+            }
         }
     },
-    mounted(){
+    created(){
         this.generateTestData();
-        this.generateThumbnailsFromTestID();
+    },
+    mounted(){
+        // this.generateThumbnailsFromTestID();
     }
 }
 </script>
