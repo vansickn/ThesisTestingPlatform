a92ba47914984f2abfd2624dee1f15d498d992e4
Noah VanSickle <vansickn@union.edu>
2021-11-09 19:19:53 -0500

    icons for representing sample type

--- src/components/TestCard.vue
+++ src/components/TestCard.vue
@@ -6,6 +6,13 @@
         <!-- Want function to ultimately be selectedThumbnail -->
         <div class="container flex flex-row justify-end mt-1">
             <h1 v-show="show_copy" class="mr-2 transition duration-200">Copied Link!</h1>
+            <button v-if="sample_type == 'Random'" class="pr-2">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3.055 11H5a2 2 0 012 2v1a2 2 0 002 2 2 2 0 012 2v2.945M8 3.935V5.5A2.5 2.5 0 0010.5 8h.5a2 2 0 012 2 2 2 0 104 0 2 2 0 012-2h1.064M15 20.488V18a2 2 0 012-2h3.064M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            </button>
+            <button v-if="sample_type == 'Fans'" class="pr-2">
+                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M17 20h5v-2a3 3 0 00-5.356-1.857M17 20H7m10 0v-2c0-.656-.126-1.283-.356-1.857M7 20H2v-2a3 3 0 015.356-1.857M7 20v-2c0-.656.126-1.283.356-1.857m0 0a5.002 5.002 0 019.288 0M15 7a3 3 0 11-6 0 3 3 0 016 0zm6 3a2 2 0 11-4 0 2 2 0 014 0zM7 10a2 2 0 11-4 0 2 2 0 014 0z"></path></svg>
+            </button>
+
             <button class="pr-2" @click="shareLink">
                 <svg class="w-6 h-6 focus:none" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"></path></svg>
             </button>
@@ -65,6 +72,7 @@ export default {
             show_copy: false,
             show_confirm_deletion: false,
             image_names: [],
+            sample_type: null,
         }
     },
     computed: {
@@ -80,6 +88,7 @@ export default {
                 this.sampleSize = doc.data().sampleSize;
                 this.numberOfImages = doc.data().numberOfImages;
                 this.title_array = doc.data().title_array;
+                this.sample_type = doc.data().sample_type;
                 this.generateThumbnailsFromTestID();
                 this.calculateTotalVotes(doc.data());
             })
