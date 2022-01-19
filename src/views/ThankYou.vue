3b61042f722f71866b83a4f01b5006de62dacd24
Noah VanSickle <vansickn@union.edu>
2022-01-19 12:30:58 -0500

    looks better now

--- src/views/ThankYou.vue
+++ src/views/ThankYou.vue
@@ -1,5 +1,6 @@
 <template>
-  Thank You for completing the survey!
+  <h1 class="text-3xl mx-auto">Thank You for Completing the survey! </h1>
+  <h1 class="text-xl mx-auto">You can expect to hear back from us for the post-experiment debrief in 3-4 weeks</h1>
 </template>
 
 <script>
