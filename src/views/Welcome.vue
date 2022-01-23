c146e32017d275782f6e9342c5df3a3845e2d7a6
Noah VanSickle <vansickn@union.edu>
2022-01-23 15:37:41 -0500

    formatting text center

--- src/views/Welcome.vue
+++ src/views/Welcome.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="container flex flex-col items-center mt-5 w-6/12" v-if="!userLoggedIn">
-    <h1 class="text-3xl">Welcome to the Experiment!</h1>
-    <span class="text-xl mt-3 text-center">We want to thank you for participating, and you will be paid $10 per hour of your time. We will keep all of your data private.</span>
+    <h1 class="text-3xl mx-auto">Welcome to the Experiment!</h1>
+    <span class="text-xl mt-3 text-center mx-auto">We want to thank you for participating, and you will be paid $10 per hour of your time. We will keep all of your data private.</span>
 
-    <span class="mt-5 text-3xl text-center">To begin, log in to your google account at the top right of the screen.</span>
+    <span class="mt-5 text-3xl text-center mx-auto">To begin, log in to your google account at the top right of the screen.</span>
   </div>
 </template>
 
