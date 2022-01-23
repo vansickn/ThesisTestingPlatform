91fe2014257326a7b90eb009b56e49ee55ae7025
Noah VanSickle <vansickn@union.edu>
2022-01-23 15:34:33 -0500

    different copy in the thank u page

--- src/views/ThankYou.vue
+++ src/views/ThankYou.vue
@@ -1,6 +1,9 @@
 <template>
   <h1 class="text-3xl mx-auto">Thank You for Completing the survey! </h1>
   <h1 class="text-xl mx-auto">You can expect to hear back from us for the post-experiment debrief in 3-4 weeks</h1>
+  <h1 class="text-xl mx-auto">If you have any questions or concerns about the experiment and its processes, please do not hesitate
+    to email me at <a href="" class="font-bold">vansickn@union.edu</a>. Thanks again!
+  </h1>
 </template>
 
 <script>
